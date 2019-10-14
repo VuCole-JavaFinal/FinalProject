74bbedd502c8a13cf55e69c757ce7831468ddf77
Cole Giles <coleagiles@gmailc.com>
2019-10-14 09:53:19 -0500

    started death screen

---
+++ src/DeathScreen.java
@@ -0,0 +1,15 @@
+public class DeathScreen {
+
+    public static void main(String[] args){
+
+        System.out.println(" __    __ ");
+        System.out.println("|  |  |  |");
+        System.out.println("|  |  |  |");
+        System.out.println("|  |__|  |");
+        System.out.println("|_      _|");
+        System.out.println("  |    |  ");
+        System.out.println("  |    |  ");
+        System.out.println("  |    |  ");
+        System.out.println("  |____|  ");
+    }
+}
