a73849a5d256027f44506e2becbc8f9a4b1f14d3
vunguyen12 <54542419+vunguyen12@users.noreply.github.com>
2019-10-15 09:49:27 -0500

    update

--- src/Attic.java
+++ src/Attic.java
@@ -5,30 +5,59 @@ public class Attic {
     public static void Attic(){
         int choice;
         Scanner input = new Scanner(System.in);
-        choice = input.nextInt();
-        System.out.println("-----------------------");
+
+        System.out.println("-------------------------------------------------------------");
+        //delay the dialog
         try {
             TimeUnit.SECONDS.sleep(3);
-            // delay 0.5 second
-//        TimeUnit.MICROSECONDS.sleep(10000);
-            // delay 1 minute
-            //TimeUnit.MINUTES.sleep(1);
         } catch (InterruptedException e) {
             System.err.format("IOException: %s%n", e);
         }
+
         System.out.println("\n you wake up in a derelict attic, with a small round window and a door on the floor.");
         System.out.println("1.Interact with window \n2.Interact with door.");
 
-        if (choice == 1){
-            System.out.println("Window is very thick and difficult to see through, look elsewhere.");
-            System.out.println("1.Interact with door");
-            choice = input.nextInt();
-                if (choice == 1){
-                    AtticDoor.AtticDoor();
+        choice = input.nextInt();
+
+            if (choice == 1) {
+                System.out.println("Window is very thick and difficult to see through, look elsewhere.\n");
+                System.out.println("1.Interact with door");
+                AtticDoor();
+        }
+            if (choice == 2) {
+                AtticDoor();
+        }
+            while(choice >= 3) {
+                System.out.println("choose again");
+
+                choice = input.nextInt();
+
+                    if (choice == 1) {
+                        AtticDoor();
+                }
+                    else if (choice == 2) {
+                         AtticDoor();
                 }
             }
-        else {
-            AtticDoor.AtticDoor();
+    }
+    public static void AtticDoor(){
+        System.out.println("Door is stuck shut  as if it had been glued shut.\n There must be some way you could open it, maybe try looking around a bit.\n");
+        System.out.println("1.Look Around \n");
+
+        boolean invalid = true;
+        while (invalid ==true) {
+            int choice;
+            Scanner input = new Scanner(System.in);
+            choice = input.nextInt();
+            if (choice == 1) {
+                System.out.println("You see some cardboard boxes in the corner, and you notice the heavily stained mattress you woke up on. \n");
+                break;
+            } else {
+                System.out.println("choose again \n");
+                invalid = true;
+            }
         }
+        System.out.println("1.Interact with boxes \n 2. Interact with");
     }
 }
+
--- src/AtticDoor.java
+++ src/AtticDoor.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class AtticDoor {
     public static void AtticDoor(){
-        System.out.println("Door is stuck shut  as if it had been glued shut.\n There must be some way you could open it, maybe try looking around a bit.");
+        System.out.println("Door is stuck shut  as if it had been glued shut.\n There must be some way you could open it, maybe try looking around a bit.\n");
+        System.out.println("1.Look Around \n");
 
+        boolean invalid = true;
+        while (invalid ==true) {
+            int choice;
+            Scanner input = new Scanner(System.in);
+            choice = input.nextInt();
+            if (choice == 1) {
+                System.out.println("You see some cardboard boxes in the corner, and you notice the heavily stained mattress you woke up on. \n");
+                break;
+            } else {
+                System.out.println("choose again \n");
+                invalid = true;
+            }
+        }
     }
 }
+
