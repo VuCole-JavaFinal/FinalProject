31012ba3fab5c45856081e3f413ed497c1ab6cf4
vunguyen12 <54542419+vunguyen12@users.noreply.github.com>
2019-10-17 09:52:25 -0500

    update classes

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,13 @@ public class Main {
                     choice = input.nextInt();
                         if (choice == 1) {
                             System.out.println("spawn in room 4, you get a key after completed a room and move on to the next one.\n" +
-                                    "   in order to win the game, you have to have enough 4 keys.\n" +
-                                    "                            Begin                           ");
+                                    "   in order to win the game, you have to have a key to the entrance door.\n");
+
+                            System.out.println(" BIG WARNNING !!!");
+                            System.out.println("-please choose a valid choices. most of the choices being chosen by pressing number."+
+                                    " If you type a word, the program will end immediately and you have to play all over again.");
+                            System.out.println("-There are some sounds (remember to turn on the volume), and images in the game. Remember to press the X button to close the image in order to continue the program");
+                            System.out.println("\n\n                            BEGIN                                   ");
                             Attic.Attic();
                             break;
                             //this statement will call to class Attic when press 1.
