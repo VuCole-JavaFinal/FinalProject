2da4393bc0ae501d0989ce215d388bdee55feed9
Cole Giles <coleagiles@gmailc.com>
2019-10-21 09:53:52 -0500

    fixed conflict

--- src/delay.java
+++ src/delay.java
@@ -3,48 +3,54 @@ import java.util.concurrent.TimeUnit;
 public class delay {
 
     //this class for delaying dialogue and picture
-    public static void delayl(){
+    public static void delayl() {
         try {
             TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                System.err.format("IOException: %s%n", e);
-            }
-            System.out.println(".");
-    }
-    public static void delay2(){
-            try {
-                TimeUnit.SECONDS.sleep(4);
-            } catch (InterruptedException e) {
-                System.err.format("IOException: %s%n", e);
-            }
+        } catch (InterruptedException e) {
+            System.err.format("IOException: %s%n", e);
         }
-    public static void delay3(){
+        System.out.println(".");
+    }
+
+    public static void delay2() {
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+    }
+
+    public static void delay3() {
         try {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             System.err.format("IOException: %s%n", e);
         }
     }
-    public static void delayforPic(){
+
+    public static void delayforPic() {
         try {
             TimeUnit.SECONDS.sleep(7);
-
-    public static void delay4(){
-        try {
-            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             System.err.format("IOException: %s%n", e);
         }
     }
+        public static void delay4(){
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                System.err.format("IOException: %s%n", e);
+            }
+        }
     public static void delay5(){
-        for(int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 5; ++i) {
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 System.err.format("IOException: %s%n", e);
+                }
+                System.out.println(".");
             }
-            System.out.println(".");
         }
-    }
 }
 
